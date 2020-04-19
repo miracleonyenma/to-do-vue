d27ab26c5237fa9b975c0fc72fb99db9ee718897
miracleonyenma <miracleficient@gmail.com>
2020-04-19 22:57:04 +0100

    update: put todo items data into external array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
+var testdata = [
+    {
+        text : "Feed the dog",
+        isDone : true
+    },
+    {
+        text : "Feed the dog again",
+        isDone : false
+    },
+    {
+        text : "Feed the dog again, again",
+        isDone : false
+    }
+]
+
 new Vue({
     el: "#app",
     data : {
-        todo : [
-            {
-                text : "Feed the dog",
-                isDone : false
-            },
-            {
-                text : "Feed the dog again",
-                isDone : false
-            },
-            {
-                text : "Feed the dog again, again",
-                isDone : true
-            }
-        ]
+        todo : testdata,
+        newTodoItem : ""
     }
 })
