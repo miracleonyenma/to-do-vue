c08ac84e8ff5393b53630de6a739cb543a652165
miracleonyenma <miracleficient@gmail.com>
2020-04-19 17:02:24 +0100

    update: get list items from vue data, use v-model directive to update task status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 new Vue({
     el: "#app",
     data : {
-        
+        todo : [
+            {
+                text : "Feed the dog",
+                isDone : false
+            },
+            {
+                text : "Feed the dog again",
+                isDone : false
+            },
+            {
+                text : "Feed the dog again, again",
+                isDone : true
+            }
+        ]
     }
 })
