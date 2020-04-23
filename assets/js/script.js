0a11e6c54a6c21d7b489881059a32c0e9c0a901c
miracleonyenma <miracleficient@gmail.com>
2020-04-23 23:33:06 +0100

    update: add css animations(buggy), add localStorage func(buggy)
    - css: elements dont yet animate smoothly, key value derived from arr count\
    - watch prop does not fire when changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,57 @@
-var testdata = [
-    {
-        text : "Feed the dog",
-        isDone : true,
-        id : 0
-    },
-    {
-        text : "Feed the dog again",
-        isDone : false,
-        id : 1
-    },
-    {
-        text : "Feed the dog again, again",
-        isDone : false,
-        id : 2
-    }
-]
+if(localStorage.getItem("testdata") == null){
+    localStorage.setItem("testdata", JSON.stringify([
+        {
+            text : "Feed the dog",
+            isDone : true,
+            id : 0
+        },
+        {
+            text : "Feed the dog again",
+            isDone : false,
+            id : 1
+        },
+        {
+            text : "Feed the dog again, again",
+            isDone : false,
+            id : 2
+        }
+    ]
+    ));
+    alert(localStorage.getItem("testdata"))
+}
+var testdata = JSON.parse(localStorage.getItem("testdata"));
+
 
 new Vue({
     el: "#app",
     data : {
         todo : testdata,
-        newTodoText : ""
+        newTodoText : "",
+        completed : testdata.reduce(function(i,item){
+            return i + (item.isDone == false)
+        }, 0)
     },
     methods : {
         add : function(){
-            this.todo.push({text: this.newTodoText, isDone:false})
-            this.newTodoText = "";
-            this.idFunc(this.todo);
+            if(this.newTodoText.trim() == ""){
+                return false
+            } else {
+                this.todo.push({text: this.newTodoText, isDone:false, id: this.todo.length});
+                this.newTodoText = "";
+            }
         }, 
         remove : function(item){            
             this.todo.splice(item, 1);         
-        },
-        idFunc : function(arr){
-            arr = arr.map((i, index) => {
-                i.id = index
-                console.log("mapping")
-            })
         }
+    },
+    watch:{
+        todo: function(){   
+            alert(JSON.stringify(this.todo));
+            localStorage.setItem("testdata", JSON.stringify(this.todo));
+            this.completed = this.todo.reduce(function(i,item){
+                return i + (item.isDone == false)
+            }, 0);
+        }, 
+        deep : true
     }
 })
