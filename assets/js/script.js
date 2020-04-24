e2c517533ec38bebbc6dae84fb6ed92f6fb205c8
miracleonyenma <miracleficient@gmail.com>
2020-04-24 16:39:45 +0100

    changes: remove with item id in index.html, object with id index in array and remove in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,5 @@
 if(localStorage.getItem("testdata") == null){
-    localStorage.setItem("testdata", JSON.stringify([
-        {
-            text : "Feed the dog",
-            isDone : true,
-            id : 0
-        },
-        {
-            text : "Feed the dog again",
-            isDone : false,
-            id : 1
-        },
-        {
-            text : "Feed the dog again, again",
-            isDone : false,
-            id : 2
-        }
-    ]
-    ));
-    alert(localStorage.getItem("testdata"))
+    localStorage.setItem("testdata", JSON.stringify([]));
 }
 var testdata = JSON.parse(localStorage.getItem("testdata"));
 
@@ -40,13 +22,15 @@ new Vue({
                 this.newTodoText = "";
             }
         }, 
-        remove : function(item){            
-            this.todo.splice(item, 1);         
+        remove : function(item){    
+            console.log(item)        
+            this.todo.splice(this.todo.findIndex(obj => {
+                return obj.id == item
+            }), 1);      
         }
     },
     watch:{
         todo: function(){   
-            alert(JSON.stringify(this.todo));
             localStorage.setItem("testdata", JSON.stringify(this.todo));
             this.completed = this.todo.reduce(function(i,item){
                 return i + (item.isDone == false)
