0833bebc4e203765e9f05618ddb66e1b25991a2f
Ogurinka Benjamin <ogurinkabenjamin@gmail.com>
2020-04-24 16:53:02 +0100

    Fix dubplicate keys bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,38 @@
-if(localStorage.getItem("testdata") == null){
-    localStorage.setItem("testdata", JSON.stringify([
-        {
-            text : "Feed the dog",
-            isDone : true,
-            id : 0
-        },
-        {
-            text : "Feed the dog again",
-            isDone : false,
-            id : 1
-        },
-        {
-            text : "Feed the dog again, again",
-            isDone : false,
-            id : 2
-        }
-    ]
-    ));
-    alert(localStorage.getItem("testdata"))
+if (localStorage.getItem("testdata") == null) {
+    localStorage.setItem("testdata", JSON.stringify([]));
+    // alert(localStorage.getItem("testdata"))
 }
 var testdata = JSON.parse(localStorage.getItem("testdata"));
-
-
 new Vue({
     el: "#app",
-    data : {
-        todo : testdata,
-        newTodoText : "",
-        completed : testdata.reduce(function(i,item){
+    data: {
+        todo: testdata,
+        newTodoText: "",
+        completed: testdata.reduce(function(i, item) {
             return i + (item.isDone == false)
         }, 0)
     },
-    methods : {
-        add : function(){
-            if(this.newTodoText.trim() == ""){
+    methods: {
+        add: function() {
+            if (this.newTodoText.trim() == "") {
                 return false
             } else {
-                this.todo.push({text: this.newTodoText, isDone:false, id: this.todo.length});
+                this.todo.push({ id: Math.floor(Math.random() * 9999) + 10, text: this.newTodoText, isDone: false });
                 this.newTodoText = "";
             }
-        }, 
-        remove : function(item){            
-            this.todo.splice(item, 1);         
+        },
+        remove: function(item) {
+            this.todo.splice(item, 1);
         }
     },
-    watch:{
-        todo: function(){   
-            alert(JSON.stringify(this.todo));
+    watch: {
+        todo: function() {
+            // alert(JSON.stringify(this.todo));
             localStorage.setItem("testdata", JSON.stringify(this.todo));
-            this.completed = this.todo.reduce(function(i,item){
+            this.completed = this.todo.reduce(function(i, item) {
                 return i + (item.isDone == false)
             }, 0);
-        }, 
-        deep : true
+        },
+        deep: true
     }
 })
